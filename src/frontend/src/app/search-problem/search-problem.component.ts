90603e421411c3981570b06b34818e95ac885e36
PhilippSchoen <80275259+PhilippSchoen@users.noreply.github.com>
2024-12-05 21:43:28 +0100

    Added actual tabs

--- src/frontend/src/app/search-problem/search-problem.component.ts
+++ src/frontend/src/app/search-problem/search-problem.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-search-problem',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './search-problem.component.html',
   styleUrl: './search-problem.component.scss'
 })
 export class SearchProblemComponent {
-
+  searchProblems: string[] = ['Pathfinding', 'Flight-Route', 'Eight-Puzzle', 'Maze'];
 }
--- src/frontend/src/app/search-tree/search-tree.component.ts
+++ src/frontend/src/app/search-tree/search-tree.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-search-tree',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './search-tree.component.html',
   styleUrl: './search-tree.component.scss'
 })
 export class SearchTreeComponent {
-
+  searchAlgorithms: string[] = [
+      'Breadth-First-Search', 'Depth-First-Search', 'Depth-Limited-Search', 'Uniform-Cost-Search', 'Bidirectional-Search', 'Greedy-Best-First-Search', 'A-Star-Search'
+  ];
 }
